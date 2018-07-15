22b003ce46b4a52ac9f583df3ddd86f2572d54d4
Jyiou Yim <kongjoe.yim@gmail.com>
2018-07-15 20:14:09 +0800

    first commit

---
+++ Joe Functions Ex3/Joe Functions Ex3/Source.cpp
@@ -0,0 +1,40 @@
+﻿#include <string.h>​
+#include <iostream>​
+​
+struct UserData​
+{
+	​
+		static int Userid; ​
+		std::string name; ​
+		int data[100000] = {}; ​
+		​
+		UserData(std::string i_name)​
+	{
+		​
+			name = i_name; ​
+			++Userid; ​
+	}; ​
+}; ​
+​
+int UserData::Userid = 0; ​
+​
+void ShowUser(UserData i_user)​
+		{
+			​
+				std::cout << i_user.Userid << std::endl; ​
+				std::cout << i_user.name.c_str() << std::endl; ​
+		}​
+			​
+				int main()​
+			{
+				​
+					UserData* pUser1 = new UserData("John"); ​
+					ShowUser(*pUser1); ​
+					UserData* pUser2 = new UserData("Mark"); ​
+					ShowUser(*pUser2); ​
+					​
+					delete pUser1; ​
+					delete pUser2; ​
+					​
+					return 0; ​
+			}​
